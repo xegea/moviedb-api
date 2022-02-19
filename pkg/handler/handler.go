3615251b8662ee073dacfee4ed0e083d5ecde564
Xavier Egea Vila <xavier.egea@gmail.com>
2022-02-19 19:25:16 +0100

    Improve getting movie parameter and error handling

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,8 +2,7 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
+	"errors"
 	"net/http"
 	"strings"
 
@@ -36,19 +35,22 @@ type Trailer struct {
 func GetMovieHandler(srv server.Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		params := mux.Vars(r)
-		objid := params["movie"]
-		fmt.Println("Query string key value", objid)
+		id, found := mux.Vars(r)["movie"]
+		if !found {
+			srv.JSON(w, http.StatusBadRequest, "request is invalid")
+		}
 
-		b, err := redis.GetRedisValue(srv.Redis.RedisJSON, objid)
+		b, err := redis.GetRedisValue(srv.Redis.RedisJSON, id)
 		if err != nil {
-			log.Println(err)
+			srv.JSON(w, http.StatusInternalServerError, err)
+			return
 		}
 
 		movie := Movie{}
 		err = json.Unmarshal(b, &movie)
 		if err != nil {
-			log.Println(err)
+			srv.JSON(w, http.StatusInternalServerError, err)
+			return
 		}
 
 		srv.JSON(w, http.StatusOK, movie)
@@ -64,20 +66,23 @@ func SetMovieHandler(srv server.Server) http.HandlerFunc {
 			return
 		}
 
-		fmt.Println(movie.Title["GB"])
-
-		redisKey := buildNetflixRedisKey(movie.Url)
+		redisKey, err := buildNetflixRedisKey(movie.Url)
+		if err != nil {
+			srv.JSON(w, http.StatusInternalServerError, err)
+			return
+		}
 
 		res, err := redis.SetRedisValue(srv.Redis.RedisJSON, redisKey, movie)
 		if err != nil {
-			log.Println(err)
+			srv.JSON(w, http.StatusInternalServerError, err)
+			return
 		}
 
 		srv.JSON(w, http.StatusOK, res)
 	}
 }
 
-func buildNetflixRedisKey(movieUrl string) string {
+func buildNetflixRedisKey(movieUrl string) (string, error) {
 
 	var redisKey string
 	redisKey = strings.Replace(movieUrl, "https://www.netflix.com", "netflix", 1)
@@ -90,12 +95,12 @@ func buildNetflixRedisKey(movieUrl string) string {
 	redisKey = strings.Replace(redisKey, "/title/", ":en-us:", 1)
 
 	if strings.Contains(redisKey, "es-en") {
-		log.Fatalf("Incorrect key %s", redisKey)
+		return "", errors.New("incorrect key " + redisKey)
 	}
 
 	if strings.Contains(redisKey, "de-en") {
-		log.Fatalf("Incorrect key %s", redisKey)
+		return "", errors.New("incorrect key " + redisKey)
 	}
 
-	return redisKey
+	return redisKey, nil
 }
